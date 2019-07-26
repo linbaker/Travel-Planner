2ae4327672e1aefd33b782eaf12c01959e7ae030
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-26 15:19:02 -0700

    Add state to app

--- src/App.js
+++ src/App.js
@@ -24,3 +24,40 @@ function App() {
 }
 
 export default App;
+
+render() {
+  const childProps = {
+    isAuthenticated: this.state.isAuthenticated,
+    userHasAuthenticated: this.userHasAuthenticated
+  };
+
+  return (
+    !this.state.isAuthenticating &&
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {this.state.isAuthenticated
+              ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+              : <Fragment>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Fragment>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes childProps={childProps} />
+    </div>
+  );
+}
