f9af732218e0079c23b6ec91518e1bad92bfddae
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-30 18:05:13 -0700

    Move avtions and reducer to local file until serverless environment working

--- src/Routes.js
+++
@@ -1,7 +0,0 @@
-export default ({ childProps }) =>
-  <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound} />
-  </Switch>;
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import flights from './flights'
-import flightsVisibilityFilter from './flightsVisibilityFilter'
-
-export default combineReducers({
-  flights,
-  flightsVisibilityFilter
-})
