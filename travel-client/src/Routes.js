2451cde3a3888590198aedad8f67bdacf7e78b67
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-31 23:33:48 -0700

    Add page not found route and files

--- travel-client/src/Routes.js
+++ travel-client/src/Routes.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomeSplash from "./containers/HomeSplash";
+import Error404 from "./containers/Error404";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={HomeSplash} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={Error404} />
   </Switch>;
---
+++ travel-client/src/containers/Error404.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Error404.css";
+
+export default () =>
+  <div className="Error404">
+    <h1>Error 404</h1>
+    <h3>Sorry, page not found!</h3>
+  </div>;
